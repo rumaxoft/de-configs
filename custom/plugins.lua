2fd6719d385228fcfc0b9c7d1c1c79c9cb6e2305
Maxim Rudenko <maximu.rudenko@gmail.com>
2024-03-25 00:33:54 +0300

    feat: remove delta from telescope-undo nvim

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -92,7 +92,6 @@ local plugins = {
     opts = {
       extensions = {
         undo = {
-          use_delta=true,
           use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
           side_by_side = false,
           diff_context_lines = vim.o.scrolloff,
